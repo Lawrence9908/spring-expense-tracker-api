7fed18203e230cf12a1f829ea571d5dcff3bd8c5
Lawrence9908 <mugwenalawrence121@gmail.com>
2023-06-11 11:56:28 +0200

    first commit

---
+++ spring-expense-tracker-api/src/main/java/com/lawrence/expensetraker/SpringExpenseTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lawrence.expensetraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExpenseTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExpenseTrackerApiApplication.class, args);
+	}
+
+}
---
+++ spring-expense-tracker-api/src/test/java/com/lawrence/expensetraker/SpringExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrence.expensetraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
