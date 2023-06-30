2b77ef0304fb39c3487d4544befc47babe392e34
Lawrence9908 <mugwenalawrence121@gmail.com>
2023-06-30 11:52:25 +0200

    Global Exception handler implementation complete

--- spring-expense-tracker-api/src/main/java/com/lawrence/expensetraker/controller/UserController.java
+++ spring-expense-tracker-api/src/main/java/com/lawrence/expensetraker/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.lawrence.expensetraker.controller;
 
+import com.lawrence.expensetraker.dto.UserDto;
+import com.lawrence.expensetraker.entity.User;
+import com.lawrence.expensetraker.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/profile")
+    public ResponseEntity<User> readUser(){
+        return new ResponseEntity<>(userService.readUser(), HttpStatus.OK);
+    }
+
+    @PutMapping("/profile")
+    public ResponseEntity<User> updateUser(@RequestBody UserDto userDto){
+        return new ResponseEntity<>(userService.updateUser(userDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deactivate")
+    public ResponseEntity<HttpStatus> deleteUser(){
+        userService.deleteUser();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- spring-expense-tracker-api/src/main/java/com/lawrence/expensetraker/exceptions/GlobalExceptionHandler.java
+++ spring-expense-tracker-api/src/main/java/com/lawrence/expensetraker/exceptions/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
@@ -28,8 +29,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorObject.setTimestamp(new Date());
         return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorObject> handleMethodArgumentNotValidException(ResourceNotFoundException ex,
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex,
                                                                       WebRequest request){
         ErrorObject errorObject = new ErrorObject();
         errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
@@ -39,7 +40,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(ItemExistsException.class)
-    public ResponseEntity<ErrorObject> handleItemExistsException(ResourceNotFoundException ex,
+    public ResponseEntity<ErrorObject> handleItemExistsException(ItemExistsException ex,
                                                                              WebRequest request){
         ErrorObject errorObject = new ErrorObject();
         errorObject.setStatusCode(HttpStatus.CONFLICT.value());
@@ -48,11 +49,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorObject, HttpStatus.CONFLICT);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObject> handleGeneralException(Exception ex,
+                                                                 WebRequest request){
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
+        return new ResponseEntity<>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request) {
+                                                                  HttpStatusCode status, WebRequest request) {
 
         Map<String, Object> body = new HashMap<>();
         body.put("statusCode", HttpStatus.BAD_REQUEST.value());
@@ -62,23 +71,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .stream()
                 .map(x-> x.getDefaultMessage())
                 .collect(Collectors.toList());
-        body.put("message", errors);
+        body.put("messages", errors);
         body.put("timestamp", new Date());
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorObject> handleGeneralException(ResourceNotFoundException ex,
-                                                                 WebRequest request){
-        ErrorObject errorObject = new ErrorObject();
-        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorObject.setMessage(ex.getMessage());
-        errorObject.setTimestamp(new Date());
-        return new ResponseEntity<>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-
 
 
 }
